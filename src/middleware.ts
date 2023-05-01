26f8ef55215a2ab98749d68c7d4ebf22164a8614
Mudit Gupta <muditg317@gmail.com>
2023-05-01 14:44:25 -0400

    implement redirection middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,8 +3,9 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 import { pageWithAlias, redirectWithAlias, Page_RedirectHub, type Redirect } from '~/content/urls'
+import { prisma } from './server/db';
 
-export function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname.substring(1);
   // =========== check for page aliases ===========
   const targetPage = pageWithAlias(pathname);
@@ -20,31 +21,44 @@ export function middleware(request: NextRequest) {
   const redirectionMatch = pathname.match(/^(redirect(s|ion)?|external)(\/([^\/]+))?$/);
   const redirection = redirectWithAlias(pathname)
       ?? [redirectionMatch?.[4]].map(pname => pname && redirectWithAlias(pname))[0];
-  if (redirection) {
+  if (redirection) { // there is an existing internal redirect saved in the config
     console.log(`redirect from [${pathname}] to [${redirection.title}](${redirection.target})`);
     return NextResponse.redirect(redirection.target);
-  } else if (redirectionMatch?.[4]) {
-    console.log(`Unknown redirection @ [${pathname}] -- [${redirectionMatch}]`);
+  } else if (redirectionMatch?.[4]) { // there is a redirection request with some target
+    console.log(`Unknown redirection @ [${pathname}] -- [${redirectionMatch[4]}] - checking database...`);
     // TODO: fetch redirections from database
-    const desiredRedirect = null as Partial<Redirect> | null;
-    if (desiredRedirect) {
+    // const desiredRedirect = null as Partial<Redirect> | null;
+    const desiredRedirect = await prisma.redirection.findFirst({
+      where: {
+        aliases: {
+          contains: `||${redirectionMatch[4]}||`
+        }
+      },
+      select: {
+        title: true,
+        target: true,
+      }
+    });
+    // console.log('DB result:', desiredRedirect);
+    if (desiredRedirect) { // the reqirection request is found in the database
       console.log(`redirect from [${pathname}] to [${desiredRedirect.title}](${desiredRedirect.target})`);
       return NextResponse.redirect(desiredRedirect.target!);
     }
     
-    console.log(`Redirecting to Redirect Hub @ [${Page_RedirectHub.pathname}]`);
-    return NextResponse.redirect(new URL(`/${Page_RedirectHub.pathname}`, request.url));
-  } else if (redirectionMatch) {
+    console.log(`Return to Redirect Hub @ [${Page_RedirectHub.pathname}]`);
+    const target = new URL(`/${Page_RedirectHub.pathname}`, request.url);
+    target.searchParams.set('from', redirectionMatch[4]);
+    return NextResponse.redirect(target);
+  } else if (redirectionMatch) { // there is a redirection request but no target
     console.log(`Broken redirection @ [${pathname}] -- [${redirectionMatch}]`);
-    if (pathname === Page_RedirectHub.pathname) {
+    if (pathname === Page_RedirectHub.pathname) { // already pointed at hub
       return NextResponse.next();
     }
-    console.log(`Redirecting to Redirect Hub @ [${Page_RedirectHub.pathname}]`);
+    console.log(`Return to Redirect Hub @ [${Page_RedirectHub.pathname}]`);
     return NextResponse.redirect(new URL(`/${Page_RedirectHub.pathname}`, request.url));
   }
 
   return NextResponse.next();
-  // return NextResponse.redirect(new URL('/about-2', request.url))
 }
 
 export const config = {
