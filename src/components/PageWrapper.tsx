bdf9300b26643617bf83a9e9ab4ef2fd20d57798
Mudit Gupta <muditg317@gmail.com>
2023-05-23 05:04:43 -0400

    single file commit just to keep log of the intersectionobserver-based scroll position detector

--- src/components/PageWrapper.tsx
+++ src/components/PageWrapper.tsx
@@ -1,21 +1,51 @@
-import React from "react";
-import { useRouter } from "next/router";
+import React, { useEffect, useState } from "react";
 // import Header from "./Header";
 import Footer from "./Footer";
 
-import { pageWithAlias } from "~/content/urls"
+import { useRouterWithPage } from "~/hooks";
+import Head from "next/head";
+import { MAIN_PAGE, navPages } from "~/content/urls";
+import NavBar from "./NavBar";
+import { PageConst } from "~/content/urls/pages";
+import { readonlyIncludes } from "~/utils/type-modifiers";
 
 type PageWrapperProps = {
   children: React.ReactNode;
 };
 const PageWrapper = ({ children }: PageWrapperProps) => {
-  const router = useRouter();
-  const currentRoute = router.pathname.substring(1);
-  // console.log(router, currentRoute);
+  const [router, currentRoute, page] = useRouterWithPage();
+  // const [activeTab, setActiveTab] = useState<string>(currentRoute);
+  const activeTab = currentRoute;
 
-  const page = pageWithAlias(currentRoute);
+  const onMainPage = page?.isMainPage ?? false;
 
-  // console.log(page);
+  // useEffect(() => {
+  //   if (!onMainPage) {
+  //     return;
+  //   }
+  //   const observer = new IntersectionObserver((entries) => {
+  //     entries.forEach((entry) => {
+  //       const tabName = entry.target.getAttribute("data-nav-tab");
+  //       console.log(tabName, entry.isIntersecting);
+  //       if (tabName != null && entry.isIntersecting) {
+  //         setActiveTab(tabName);
+  //       }
+  //     });
+  //   }, { rootMargin: "-50% 0px -50% 0px" });
+  //   // (window as any).sat = setActiveTab;
+  //   // (window as any).observer = observer;
+
+  //   const navTabSections = document.querySelectorAll("[data-nav-tab]");
+  //   console.log(navTabSections);
+
+  //   navTabSections.forEach((section) => {
+  //     observer.observe(section);
+  //   });
+
+  //   return () => {
+  //     observer.disconnect();
+  //   }
+  // }, [onMainPage]);
 
   if (!page) {
     return <>
@@ -23,29 +53,60 @@ const PageWrapper = ({ children }: PageWrapperProps) => {
     </>
   }
 
-  return (
-    <main className="flex selection:bg-zinc-200/30 flex-col overflow-x-hidden min-h-screen items-center bg-zinc-100 dark:bg-zinc-900 font-clash max-h-auto relative">
+
+  // if (page.isMainPage) {
+  //   return <>
+  //     {children}
+  //   </>
+  // }
+
+  return (<>
+    <Head>
+      <title key='title'>{page.isMainPage ? page.title : `${MAIN_PAGE?.title} - ${page.title}`}</title>
+      <meta key='desc' name="description" content="Mudit Gupta website" />
+      {/* <link rel="icon" href="/favicon.ico" /> */}
+      <link key='icon-svg' rel="icon" href="favicon.svg"
+        type="image/svg+xml" />
+      <link key='ico-light' rel="icon" href="favicon-light.ico"
+        media="(prefers-color-scheme: light)"
+      />
+      <link key='ico-dark' rel="icon" href="favicon-dark.ico"
+        media="(prefers-color-scheme: dark)"
+      />
+    </Head>
+    <div className="flex selection:bg-zinc-200/30 flex-col overflow-x-hidden min-h-screen items-center bg-zinc-100 dark:bg-zinc-900 font-clash max-h-auto relative">
       {/* <Palette /> used for KBar */}
-      <div className="flex w-full h-full lg:w-[60%] md:w-2/3">
-        <div className="w-[6%] fixed left-0 h-full z-50 hidden lg:block md:block">
-          {/* <NavBar path={currentRoute} /> */}
-        </div>
-        <div className="fixed top-0 w-full z-50 block lg:hidden md:hidden px-8 pt-4">
+      <header className="">
+        <nav className="w-[6%] fixed left-0 h-full z-50 hidden lg:block md:block">
+          <NavBar<PageConst>
+              activeTab={activeTab}
+              entries={[
+                MAIN_PAGE,
+                ...navPages
+              ] as const}
+              isActiveTab={(entry: PageConst) => readonlyIncludes(entry.aliases, activeTab)}
+              onTabClick={(entry: PageConst) => void router.push(entry.aliases[0])}
+            />
+        </nav>
+        <nav className="fixed top-0 w-full z-50 block lg:hidden md:hidden px-8 pt-4">
           {/* <MobileNavBar path={currentRoute} /> */}
-        </div>
-        <div className="w-full min-h-screen h-full p-8 flex flex-col items-center relative">
-          <section className="flex flex-col w-full justify-between mt-16 lg:mt-0 md:mt-0 prose">
+        </nav>
+      </header>
+      <div className="w-full min-h-screen h-full flex flex-col items-center relative">
+        <section className="flex flex-col w-full justify-between mt-16 lg:mt-0 md:mt-0 prose">
+          {/* <Header /> -- only for main page */}
+          {/* {!page.isMainPage && <>
             <h1 className="dark:text-zinc-200 text-zinc-900 leading-none mb-3">{page?.title ?? "no title"}</h1>
             <p className="dark:text-zinc-400 text-zinc-800 m-0 leading-tight">
               {page?.description ?? "no description"}
             </p>
-            { children }
-          </section>
-          <Footer />
-        </div>
+          </>} */}
+          { children }
+        </section>
       </div>
-    </main>
-  );
+      <Footer />
+    </div>
+  </>);
 };
 
 export default PageWrapper;
