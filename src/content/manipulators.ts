628e91414f60b31cae4a4f945e02d278cdbd900e
Mudit Gupta <muditg317@gmail.com>
2023-05-26 09:02:03 -0400

    somewhat cleanup manipulator file

--- src/content/manipulators.ts
+++ src/content/manipulators.ts
@@ -1,51 +1,47 @@
 import { readonlyFilter } from "~/utils/type-modifiers";
 import type { ElementOf, IndicesOf, TuplifyUnion } from "~/utils/types";
 
-// function withByFieldMember<const C, Func extends <F extends keyof C, V extends C[F]>(f:F, v: V) => unknown>(data: ReadonlyArray<C>, func: Func) {
-function withByFieldMember<const C, Func extends <F extends keyof C, V extends C[F]>(f: F, v: V) => ReturnType<Func>>(data: ReadonlyArray<C>, func: Func) {
-  return Object.assign(func, {
-    byField: 
-       function withByFieldMemberFor<const F extends keyof C>(flagName: F) {
-        return function withByFieldMemberForField<const V extends C[F]>(value: V) {
-          type Result =
-            Func extends (f:F, v: V) => infer R
-              ? R
-              : typeof func<F,V> extends ((f:F, v: V) => infer R)
-                ? typeof func<F,V>
-                : false;//`Error: ${Exclude<F, symbol>} ${Extract<V, string|number>} -- ${ReturnType<typeof func<F,V>>}`;
-          // type Result = ReturnType<typeof func<F, V>>;
-          return func<F,V>(flagName, value) as Result;
-        }
-      }
-      // <const F extends keyof C>(flagName: F) => func.bind(null, flagName),
-  });
-}
+type C_<$T extends {c:unknown}> = $T['c'];
+type F_<$T extends {f:unknown}> = $T['f'];
+type V_<$T extends {v:unknown}> = $T['v'];
 
-type Transformer = { c: unknown, f: unknown, v: unknown, type: unknown }
-type apply<$T extends Transformer, C, F, V> = ($T & {c:C,f:F,v:V})['type'];
-// interface $With extends Transformer { type: TuplifyUnion<Extract<this['c'], {[K in this['f']]: this['v']}>>[0] }
-// type foo = apply<$With, {a:1,b:2}|{a:2,b:5}, 'a', 2>;
+type Transformer = { c: unknown, f: unknown, v: unknown, type: unknown };
+type apply_$T<$T extends Transformer, C, F, V> = ($T & {c:C,f:F,v:V})['type'];
 
-function withField2<
+type FieldValidator = { c: unknown, type: unknown };
+type apply_$FV<$FV extends FieldValidator, C> = keyof C & ($FV & {c:C})['type'];
+
+function withField<
   $transformer extends Transformer,
   const C,
   Fn extends <F extends keyof C, V extends C[F]>(f:F, v:V) => unknown,
 >(func: Fn) {
   return Object.assign(func, {
-    byField: <F extends keyof C>(flagName: F) => <V extends C[F]>(value: V) => func(flagName, value) as apply<$transformer, C, F, V>,
+    byField: <F extends keyof C>(flagName: F) => <V extends C[F]>(value: V) => func(flagName, value) as apply_$T<$transformer, C, F, V>,
   });
 }
-// function withData<
-//   $transformer extends Transformer,
-//   const C,
-//   Fn extends <const C, F extends keyof C, V extends C[F]>(c:C,f:F,v:V) => unknown,
-// >(data: ReadonlyArray<C>, func: Fn) {
-//   return function method<F extends keyof C, V extends C[F]>(f:F,v:V) {
-//     return func(data, f, v) as apply<$transformer, C, F, V>;
-//   }
-// }
+function withData<
+  $transformer extends Transformer,
+  // const C,
+  // $fieldValidator extends FieldValidator,
+  Fn extends <const C_fn, const F extends keyof C_fn, const V extends C_fn[F]>(d:ReadonlyArray<C_fn>,f:F,v:V) => unknown
+  // Fn extends (...args: Parameters<Fn>) => ReturnType<Fn>,
+>(func: Fn) {
+  return function withData<const C>(data: ReadonlyArray<C>) {
+    function funcOnData<const F extends keyof C, const V extends C[F]>(flagName: F, value: V) {
+      return func(data, flagName, value);// as apply_$T<$transformer, C, F, V>;
+    }
+    return withField<$transformer, C, typeof funcOnData>(funcOnData);
+  }
+  // return Object.assign(func, {
+  //   withData,
+  // });
+}
 
-function filterData<const C, const F extends keyof C, const V extends C[F]>(data: ReadonlyArray<C>, flagName: F, value: V) {
+type UnknownArray = Array<unknown>|ReadonlyArray<unknown>;
+type ArrayValuedKeys<C> = { [K in keyof C]: C[K] extends UnknownArray ? K : never }[keyof C];
+type NonArrayValuedKeys<C> = Exclude<keyof C, ArrayValuedKeys<C>>;
+function filterData<const C, const F extends NonArrayValuedKeys<C>, const V extends C[F]>(data: ReadonlyArray<C>, flagName: F, value: V) {
   type FilteredItem = Extract<C, {[K in F]: V}>;
   function filterFunc(item: C): item is FilteredItem {
     return item[flagName] === value;
@@ -53,49 +49,42 @@ function filterData<const C, const F extends keyof C, const V extends C[F]>(data
   return readonlyFilter(data, filterFunc);
 }
 function filterFor<const C>(data: ReadonlyArray<C>) {
-  return function filteredData<const F extends keyof C, const V extends C[F]>(flagName: F, value: V) {
+  return function filteredData<const F extends NonArrayValuedKeys<C>, const V extends C[F]>(flagName: F, value: V) {
     return filterData(data, flagName, value);
   }
 }
 
 
-function firstWithFromData<const C, const F extends keyof C, const V extends C[F]>(data: ReadonlyArray<C>, flagName: F, value: V) {
-  type FilteredItem = Extract<C, {[K in F]: V}>;
-  function filterFunc(item: C): item is FilteredItem {
+interface $FV_firstWithFromData extends FieldValidator {
+  type: NonArrayValuedKeys<C_<this>>
+}
+function firstWithFromData<const Const, const Field extends keyof Const, const Val extends Const[Field]>(data: ReadonlyArray<Const>, flagName: Field, value: Val) {
+  type FilteredItem = Extract<Const, {[K in Field]: Val}>;
+  function filterFunc(item: Const): item is FilteredItem {
     return item[flagName] === value;
   }
   return data.find(filterFunc) as TuplifyUnion<FilteredItem>[0];
 }
-interface $firstWithFromData extends Transformer {
+interface $T_firstWithFromData extends Transformer {
   type: ReturnType<typeof firstWithFromData<
-    this['c'],
-    this['f'] extends keyof this['c'] ? this['f'] : never,
-    this['v'] extends this['c'][
-        this['f'] extends keyof this['c'] ? this['f'] : never
-      ] ? this['v'] : never
+    C_<this>,
+    F_<this> extends keyof C_<this> ? F_<this> : never,
+    V_<this> extends C_<this>[
+        F_<this> extends keyof C_<this> ? F_<this> : never
+      ] ? V_<this> : never
   >>
 }
+const genericFirstWith = withData<
+  $T_firstWithFromData,
+  // $FV_firstWithFromData,
+  typeof firstWithFromData
+>(firstWithFromData);
 
 function firstWithFuncFor<const C>(data: ReadonlyArray<C>) {
   function where<F extends keyof C, V extends C[F]>(flagName: F, value: V) {
     return firstWithFromData(data, flagName, value);
   }
-  // type t<F extends keyof C, V extends C[F]> = ReturnType<typeof where>;
-  
-  // type f<F,V> = apply<$transformer, C, F, V>;
-  // return where as <F extends keyof C, V extends C[F]>(flagName: F, value: V) => apply<$firstWithInData, C, F, V>;
-  // return withByFieldMember(data, where);
-  return withField2<$firstWithFromData, C, typeof where>(where);
-  // return Object.assign(where, {
-  //   byField: <F extends keyof C>(flagName: F) => <V extends C[F]>(value: V) => where(flagName, value),
-  // } as const);
-  // return Object.assign(where, {
-  //   byField: function byField<F extends keyof C>(flagName: F) {
-  //     return function firstWithFieldValue<V extends C[F]>(value: V) {
-  //       return where(flagName, value);
-  //     }
-  //   }
-  // } as const);
+  return withField<$T_firstWithFromData, C, typeof where>(where);
 }
 
 
@@ -111,18 +100,11 @@ function excludingFuncFor<const C>(data: ReadonlyArray<C>) {
     return excludingFromData(data, flagName, value);
   }
 }
-function firstWithFieldValueMetaFuncFor<const C>(data: ReadonlyArray<C>) {
-  return function firstWithFieldSlot<F extends keyof C>(flagName: F) {
-    return function firstWithFieldValue<V extends C[F]>(value: V) {
-      return firstWithFromData(data, flagName, value);
-    }
-  }
-}
 
 export function filterFuncsFor<const C>(data: ReadonlyArray<C>) {
   return Object.assign(
     filterFor(data), {
-      firstWith: firstWithFuncFor(data),
+      firstWith: genericFirstWith(data),//firstWithFuncFor(data),
       excluding: excludingFuncFor(data),
     } as const
   );
@@ -149,3 +131,4 @@ export type ValidateListField<C, F extends keyof C, L extends ReadonlyArray<C>, 
     : Reused<C, L, FieldUnions<L, C, F>> extends never
       ? InterfaceFollowingList<GetFieldValues<C, F, L>, InvalidV, C>
     : `data list cannot repeat items in ${F extends symbol ? `symbol` : F} field: found "${Reused<C, L, FieldUnions<L, C, F>>}"`;
+
